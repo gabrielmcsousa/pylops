bdc5c96ffec4982cc5aed98d1063e94d1f82b94c
Gabriel Mascarenhas <gabrielmascsousa@gmail.com>
2025-01-02 17:21:32 -0300

    First draft of installation script for pylops and devito with fixed dependencies created.

---
+++ install_pylops_devito.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+FOLDER_SET=false
+
+echo "*** Creating Pylops Conda Environment ***"
+conda env create -f environment-dev.yml
+echo "*** Activating Pylops Conda Environment ***"
+conda activate pylops #NOT WORKING PROPERLY!!!!
+echo "*** Installing Pylops ***"
+pip install -e .
+
+echo "*** Scaning for DEVITO source codes... ***"
+# Scan HOME and subfolders for available devito source codes.
+for FILE in $(grep -rl --include "*.py" --exclude-dir=".vscode-server" "name='devito'" $HOME 2>/dev/null)
+do
+    FOLDER=${FILE%/setup.py}
+    echo "DEVITO source found in >> $FOLDER"
+    read -p "Install devito from the above directory? (Y/N):" confirm
+    case "$confirm" in
+        y|Y )
+            FOLDER_SET=true
+            break
+            ;;
+        * ) continue;;
+    esac
+done
+
+# Check if devito folder was set and continues installation.
+if $FOLDER_SET ; then
+    echo "Folder set! Continuing installation."
+else
+    echo "Devito folder not set. The script will now exit!"
+    exit 1
+fi
+
+cd $FOLDER
+echo "*** Installing Devito ***"
+pip install -e .
+
+echo "*** Fixing Incompatible Libraries ***"
+pip install flake8==7.1.1 numba==0.60.0 numpy==1.26.4 numpydoc==1.7.0 scipy==1.14.1 sympy==1.13.3
